43a99c7e9ff7e5c3df7a7c959417e16f21756078
Anthony Gonzalez <tony.zlzg@gmail.com>
2022-11-23 22:22:57 -0600

    actualizacion de archivo message.php

--- message.php
+++ message.php
@@ -1,7 +1,10 @@
 <?php
 // conexion a la base de datos
-$conn = mysqli_connect("localhost:3308", "root", "sony", "jaguarbot") or die("Database Error");
 
+
+//require_once "connection.php";
+$conn = mysqli_connect("localhost:3308", "root", "sony", "jaguarbot") or die("Database Error");
+//$conn =connection();
 // obteniendo el mensaje del usuario a través de ajax
 $getMesg = mysqli_real_escape_string($conn, $_POST['text']);
 
@@ -17,7 +20,24 @@ if (mysqli_num_rows($run_query) > 0) {
     $replay = $fetch_data['replies'];
     echo $replay;
 } else {
-    echo "¡Lo siento, no puedo ayudarte con este inconveniente! Favor comunícate con el administrador en el siguiente enlace:
+
+    //si la pregunta no tiene respuesta, se insertara la consulta en la base de datos
+    registrar($getMesg, $conn);
+    echo "¡Lo siento, no puedo ayudarte con este inconveniente! por favor comunícate con el administrador en el siguiente enlace:
     
     </br><a href='https://cdhidalgo.tecnm.mx/' target='blank'>Contacto</a>";
+}
+
+
+//funcion  para registrar preguntas que no fueron contestadas
+function registrar($mensaje, $conn){
+
+    //obteniendo el mensaje del usario
+    $msg = $mensaje;
+    //insertar 
+    $stmt = $conn->prepare("INSERT INTO jaguarbot (queries) VALUES (?)");
+    $stmt->bind_param('s', $msg);
+    $stmt->execute();
+    $stmt->close();
+   
 }
